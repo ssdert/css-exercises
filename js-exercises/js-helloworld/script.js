e833498a33e9b7d8b7c5900f469841a0ad3fe8c1
owen <44922856+ssdert@users.noreply.github.com>
2019-05-13 17:26:32 +0200

    js

--- js-exercises/js-helloworld/script.js
+++ js-exercises/js-helloworld/script.js
@@ -22,5 +22,61 @@ var myMessage = messageToTheWorld + ' from ' + myName;
 
 // try to substitute the content of the h1 element
 var titleElement = document.getElementById('title');
-console.log(titleElement);
+/* console.log(titleElement); */
+// innerHTML manipulates the content between the HTML tags
 titleElement.innerHTML = myMessage;
+
+/*****************
+ * Manipulate CSS
+ ****************/
+// select the element with the ID 'styleButton'
+var buttonStyleElement = document.getElementById('styleButton');
+
+// console.log(myButton);
+// console.log("This is the element" + myButton);
+// console.log(buttonStyleElement);
+// buttonElement.classList.add('myButton');
+
+
+
+// add event listener .addEventListener(type, function)
+buttonStyleElement.addEventListener('click', changeTextColor);
+
+// a varible storing a Number
+var counter = 0;
+
+// function containing some code
+function changeTextColor() {
+
+  var titleColors = ["red", "purple", "pink", "orange", "green"];
+
+  // print the Whole Array
+  console.log(titleColors);
+  // print the Array length
+  console.log(titleColors.length);
+  // print the first item in the Array
+  console.log(titleColors[0]);
+  //print the last item in the Array
+  console.log(titleColors[titleColors.length -1]); 
+
+  // assign a new value to the color property of the element
+  titleElement.style.color = titleColors[counter];
+  
+  // == comparing the value
+  // === comparing the data
+
+  if(counter == titleColors.length - 1 ) {
+    console.log("WOOHOO IT IS TRUE");
+    counter = -1;
+  }
+
+  // increment by one
+  counter ++;
+
+  console.log('Color changed');
+};
+
+// execute a function
+//changeTextColor();
+
+
