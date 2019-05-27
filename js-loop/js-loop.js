0bf02a973280e5020f543d6f187135c95b45ed0e
owen <44922856+ssdert@users.noreply.github.com>
2019-05-28 00:01:25 +0800

    loop

--- js-loop/js-loop.js
+++ js-loop/js-loop.js
@@ -22,8 +22,53 @@ var allTheNames = [];
 var nameButton = document.getElementById('nameButton');
 var nameInput = document.getElementById('myName');
 var messageBox = document.getElementById('messageBox');
+var undoButton = document.getElementById('undoButton');
+var showButton = document.getElementById('showButton')
 
 nameButton.addEventListener('click', sayMyName);
+undoButton.addEventListener('click', undo);
+showButton.addEventListener('click', showAllName)
+
+/*  console.log = function(message) {
+  document.getElementById('nameBox').innerHTML = message;
+};  */
+
+
+function showAllName() {
+  document.getElementById('nameBox').innerHTML = allTheNames;
+}
+
+
+/* document.getElementById('myName').value = ''; */
+
+/* document.getElementById('nameBox').innerHTML = allTheNames; */
+
+function undo() {
+
+  if( nameInput.value.length = 0 ){
+    updateTheMessage(
+      "Successfully Undo", false);
+  } else {
+    updateTheMessage(
+      "Successfully Undo", true);
+  }
+  allTheNames.pop();
+  document.getElementById('nameBox').innerHTML = allTheNames;
+  
+}
+
+{/*
+<button onclick="document.getElementById('myInput').value = ''">Clear input field</button>
+
+<input type="text" value="Blabla" id="myInput"></input>
+
+<script language="javascript">
+function ShowMeDate() {
+　var Today=new Date();
+　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
+}
+</script>
+<button onclick="ShowMeDate()">告訴我今天日期</button> */}
 
 function sayMyName() {
   
@@ -37,12 +82,12 @@ function sayMyName() {
     // put the cursor in the input field 
     nameInput.focus();
 
-    // pick the la
+    // pick the last name from the array
     var lastNameAdded = allTheNames[allTheNames.length - 1];
     // SUCCESS
     updateTheMessage(
       "You have successfuly added " + lastNameAdded + " to the list", true);
-/*     console.log(allTheNames[allTheNames.length - 1]); */
+/*      console.log(allTheNames[allTheNames.length - 1]);  */
     /* console.log(allTheNames); */
   } else {
     /* console.log('No name'); */
