61a9a634af391a2adef749fbed61fce05312ecec
owen <44922856+ssdert@users.noreply.github.com>
2020-04-24 01:02:18 +0800

    2020/4/24

--- exam-4/script.js
+++ exam-4/script.js
@@ -1,16 +1,34 @@
 var elements = document.getElementsByClassName("column");
+var line = document.getElementById("line");
+var line1 = document.getElementById("line1");
+var line1 = document.getElementById("line2");
+var line1 = document.getElementById("line3");
+var line1 = document.getElementById("line4");
+var line1 = document.getElementById("line5");
     
 var i;
 
 function listView() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.width = "100%";
+    document.getElementById("line").style.visibility = "visible";
+    document.getElementById("line1").style.visibility = "visible";
+    document.getElementById("line2").style.visibility = "visible";
+    document.getElementById("line3").style.visibility = "visible";
+    document.getElementById("line4").style.visibility = "visible";
+    document.getElementById("line5").style.visibility = "visible";
   }
 }
 
 function gridView() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.width = "50%";
+    document.getElementById("line").style.visibility = "hidden";
+    document.getElementById("line1").style.visibility = "hidden";
+    document.getElementById("line2").style.visibility = "hidden";
+    document.getElementById("line3").style.visibility = "hidden";
+    document.getElementById("line4").style.visibility = "hidden";
+    document.getElementById("line5").style.visibility = "hidden";
   }
 }
 
